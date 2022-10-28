857e5d18b6d05e3947e931c70061949f62060530
Milan <you@example.com>
2022-10-28 11:05:16 +0200

    commit

---
+++ include/psl-config.php
@@ -0,0 +1,14 @@
+<?php
+
+ 
+   
+define("HOST", "localhost");     
+define("USER", "root");     
+define("PASSWORD", "");    
+define("DATABASE", "gym");    
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    
+?>
